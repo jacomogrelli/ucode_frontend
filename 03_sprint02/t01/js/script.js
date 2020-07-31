e8c5419a02ae5114025232206e333b703ec06c10
jacomogrelli <nechaeff@gmail.com>
2020-07-31 14:55:14 +0300

    evening commit

---
+++ 03_sprint02/t01/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+/*
+ * let   - local variable
+ * const - local constanta
+ * var   - not using in modern scripts
+ */
+
+let messege = "Hello JavaScript from outside!"
+
+alert(messege);
