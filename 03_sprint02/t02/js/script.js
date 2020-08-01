a159223b3e370e0d7d99f24033adf9d6d7362dda
jacomogrelli <nechaeff@gmail.com>
2020-08-01 12:54:28 +0300

    evening commit

---
+++ 03_sprint02/t02/js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var quote = "";
+
+alert(quote);
+quote = "With great power, comes great responsibility.";
+alert(quote);
+
+let displayAuthor = () => {
+    let author = "Spiderman";
+    alert(author);
+    quote = "...";
+}
+displayAuthor()
+alert(quote);
