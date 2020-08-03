dade6dca7dba84b3ebdde2cc3d76491df5b0f725
jacomogrelli <nechaeff@gmail.com>
2020-08-03 16:09:15 +0300

    evening commit

---
+++ 03_sprint02/newtask.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+mkdir t$1
+mkdir t$1/js
+touch t$1/index.html t$1/js/script.js
---
+++ 03_sprint02/t05/js/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let str1 = "You're catnip to a girl like me. Handsome, dazed,and to die for...";
+
+let str1Lenth = str1.length;
+console.log(str1Lenth);
+console.log(str1[5]);
+console.log(str1.toUpperCase());
+
+let str2 = `Laurel Hedare: "Game over."`;
+let str3 = `Selina Kyle: catwoman"Mmm seemsCatwomanlike everyCatWomanwoman you
+            try to savewindsCatWOMANup dead... or deeply resentful."`;
+substr = ['Cc']['Aa']['Tt']['Ww']['Oo'][]
+console.log(str3.replace(substr, " "));
