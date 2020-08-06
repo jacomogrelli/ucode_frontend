9db46e10bee50c76018ac316c512ae505e3d7890
jacomogrelli <nechaeff@gmail.com>
2020-08-06 15:32:57 +0300

    t12

---
+++ 03_sprint02/t12/js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let triangle = (count) => {
+  let result = "";
+
+  for(let i = 1; i <= count; i++) {
+    for(let j = 0; j < i; j++) {
+      result += "*";
+    }
+    result += "\n";
+  }
+  return result;
+}
+
+alert(triangle(6));
