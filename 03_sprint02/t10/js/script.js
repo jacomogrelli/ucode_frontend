9163906a406f18f84d872e0897769919d665933f
jacomogrelli <nechaeff@gmail.com>
2020-08-06 08:45:51 +0300

    evening commit

--- 03_sprint02/t10/js/script.js
+++ 03_sprint02/t10/js/script.js
@@ -1,2 +1,13 @@
 "use strict"
 
+let makeHeroName = () => {
+  let animal, gender, age, name;
+
+  animal = prompt("What animal is the superhero most similar to?\nInput \
+  usage: Max 20 symbols, only one word, only letters");
+  console.log(animal);
+
+
+}
+
+makeHeroName();
