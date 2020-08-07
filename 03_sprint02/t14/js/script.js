61db117657464b03ada02b65d5c9ad3eba6fc073
jacomogrelli <nechaeff@gmail.com>
2020-08-07 18:17:29 +0300

    t14-15

--- 03_sprint02/t14/js/script.js
+++ 03_sprint02/t14/js/script.js
@@ -1,2 +1,14 @@
 "use strict"
 
+let total = (addCount, addPrice, curentTotal) => {
+  // return +((curentTotal !== undefined) ? curentTotal + addCount *
+  //   addPrice : addCount * addPrice).toFixed(2);
+  let result = 0;
+
+  if (curentTotal === undefined) {
+    curentTotal = 0;
+  }
+  result = curentTotal + addCount * addPrice;
+  result = Number.result.toFixed(2);
+  return result;
+}
---
+++ 03_sprint02/t15/js/script.js
@@ -0,0 +1,54 @@
+"use strict"
+
+let checkDivision = (beginRange, endRange) => {
+  let str = "", flagEven = false, flagThree = false, flagTen = false;
+
+  for (; +beginRange <= +endRange; beginRange = String(+beginRange + 1)) {
+    str += beginRange;
+    if (+beginRange % 2 === 0) {
+      str += ` is even`;
+      flagEven = true
+    }
+    if (+beginRange % 3 === 0) {
+      if (flagEven)
+        str += `,`;
+      str += ` is multiple of 3`;
+      flagThree = true;
+    }
+    if (+beginRange % 10 === 0) {
+      if (flagEven || flagThree)
+        str += `,`
+      str += ` is divisible by 10`;
+      flagTen = true;
+    }
+    if (!flagEven && !flagTen && !flagThree)
+      str += ` -`;
+    str += `\n`;
+    flagEven = false;
+    flagThree = false;
+    flagTen = false;
+  }
+  console.log(str);
+}
+
+let intMain = () => {
+  let getBegin = "50"; //prompt("Input the berin of the range")
+  let getEnd = "60"; //prompt("Input the begin of the range")
+
+  if (getBegin.length == 0) {
+    console.log(`Default value "1" was setted up`);
+    getBegin = "1";
+  }
+  if (getEnd.length == 0) {
+    console.log(`Default value "100" was setted up`);
+    getEnd = "100";
+  }
+  if (+getBegin == NaN || +getEnd == NaN) {
+    console.log("Wrong input, try again");
+    intMain();
+  }
+  if (+getBegin <= +getEnd)
+    checkDivision(+getBegin, +getEnd);
+}
+
+intMain();
