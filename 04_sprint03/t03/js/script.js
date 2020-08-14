e7c0bf7bb0a78a66c73db1343506449926eef202
jacomogrelli <nechaeff@gmail.com>
2020-08-14 10:46:58 +0300

    t03

--- 04_sprint03/t03/js/script.js
+++ 04_sprint03/t03/js/script.js
@@ -3,41 +3,54 @@
 // function that delete duplicates in array
 let delDup = (arr) => arr.filter((v, i) => arr.indexOf(v) === i);
 
-// function that delete extra spaces in the string and split array to string
-let delExtraSpaces = (str, wrds = " ") => {
-  ((str += ' ' + wrds).trim()).split(/\s+/);
+// function that delete extra spaces in the string and split to array of strings
+let delExtraSpaces = (str = ` `, wrds = ` `) => {
+  return ((str += ' ' + wrds).trim()).split(/\s+/);
 }
 
-let addWords = (obj, wrds = " ") => {
+let addWords = (obj, wrds) => {
   if (obj) {
     let arr = delExtraSpaces(obj.words, wrds);
     obj.words = (delDup(arr)).join(` `);
   }
 }
 
-let removeWords = (obj, wrds = "") => {
+let removeWords = (obj, wrds) => {
   if (obj) {
-    let arr_obj = delDup(delExtraSpaces(obj.words));
-    let arr_wrds = delDup(delExtraSpaces(wrds))
-    arr_wrds.forEach(element => {
-      (arr_obj.indexOf(element) === -1)
-      ? 0 :arr_obj.splice(0, arr_obj.indexOf(element))
-    });
-    console.log(arr_obj, arr_wrds);
+    let arrObj = delDup(delExtraSpaces(obj.words));
+    let arrWrds = delDup(delExtraSpaces(wrds));
 
-    // obj.words = (delDup(arr)).join(` `);
+    arrWrds.forEach(element => {
+      (arrObj.indexOf(element) === -1)
+        ? 0 : arrObj.splice(arrObj.indexOf(element), 1)
+    });
+    obj.words = arrObj.join(` `)
   }
 }
 
-const obj = {
-  words: '  newspapers newspapers  books magazines'
-};
+let changeWords = (obj, oldWrds = ' ', newWrds = ' ') => {
+  if (obj) {
+    let arrObj = delDup(delExtraSpaces(obj.words));
+    let arrOldWrds = delDup(delExtraSpaces(oldWrds));
+    let arrNewWrds = delDup(delExtraSpaces(newWrds));
+
+    arrOldWrds.forEach(element => {
+      (arrObj.indexOf(element) === -1)
+        ? 0 : arrObj.splice(arrObj.indexOf(element), 1)
+    });
+    obj.words = arrObj.concat(arrNewWrds).join(" ");
+  }
+}
+
 
 // // test cases
+// const obj = {
+//   words: '  newspapers newspapers  books magazines  '
+// };
 // console.log(obj); // {words: "newspapers newspapers  books magazines"}
-addWords(obj, 'radio newspapers');
-console.log(obj); // {words: "newspapers books magazines radio"}
-removeWords(obj, 'newspapers   radio');
+// addWords(obj, 'radio newspapers');
+// console.log(obj); // {words: "newspapers books magazines radio"}
+// removeWords(obj, 'newspapers   radio');
 // console.log(obj); // {words: "books magazines"}
 // changeWords(obj, 'books radio  magazines', 'tv internet');
 // console.log(obj); // {words: "tv internet"}
