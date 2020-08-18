0319955233b673841e1fe77637bf059b8761c625
jacomogrelli <nechaeff@gmail.com>
2020-08-18 17:55:31 +0300

    t07

--- 04_sprint03/t07/js/script.js
+++ 04_sprint03/t07/js/script.js
@@ -1,27 +1,35 @@
-"use strict"
+"use strict";
 
 let correctErrors = (character) => {
-  for (let i in character) {
-    if (character[i].getAttribute(`class`) === undefined)
-    character[i].setAttribute(`class`, `unknown`);
-    if (character[i].getAttribute(`data-element`) === undefined)
+  for (let i = 0; i < character.length; i++) {
+    if (character[i].getAttribute(`data-element`) === null)
       character[i].setAttribute(`data-element`, `none`);
+    if (character[i].getAttribute('class') === null)
+      character[i].setAttribute(`class`, `unknown`);
   }
-}
+};
 
-
+let makeCircle = (character) => {
+  for (let i = 0; i < character.length; i++) {
+    let dataElem = (character[i].getAttribute(`data-element`)).split(` `);
+    character[i].appendChild(document.createElement(`br`));
+    for (let j = 0; j < dataElem.length; j++) {
+      let node = document.createElement(`div`);
+      node.className = `elem ${dataElem[j]}`;
+      if (dataElem[j] === `none`) {
+        let line = document.createElement('div');
+        line.className = `line`;
+        node.appendChild(line);
+      }
+      character[i].appendChild(node);
+    }
+  }
+};
 
 let rendering = () => {
   let character = document.body.getElementsByTagName(`li`);
   correctErrors(character);
-  console.log(character);
-  // console.log(typeof character);
-  // console.log(character[0].attributes);
-  // console.log((character[7].attributes.class));
-
-
-
-
-}
+  makeCircle(character);
+};
 
 rendering();
