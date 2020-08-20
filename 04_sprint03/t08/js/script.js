1bdd2a9be52f8cddf33c331b45561756af483dfb
jacomogrelli <nechaeff@gmail.com>
2020-08-20 11:59:42 +0300

    evening commit

--- 04_sprint03/t08/js/script.js
+++ 04_sprint03/t08/js/script.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 let heroesList = [
   {name: 'Black Panter', strength: 66, age: 53,},
   {name: 'Captain America', strength: 79, age: 137,},
@@ -13,10 +12,12 @@ let heroesList = [
   {name: 'Yon-Rogg', strength: 73, age: 52,},
 ]
 
-/* Function makeNotification render notification div with the parameters:
+/*
+ * Function makeNotification render notification div with the parameters:
  * order == start - generate starting div with the info how to sort table
  * order == ACS || DESC generate after sorting notification with the order
- * of sorting and witch parameter was used */
+ * of sorting and witch parameter was used
+ */
 let makeNotification = (order, param) => {
   let divNotification = document.getElementById(`notification`);
   let node = document.createElement(`span`);
@@ -28,25 +29,65 @@ let makeNotification = (order, param) => {
   divNotification.appendChild(node);
 }
 
+/*
+ * Function that make table markup, fill header with buttons
+ * Size of table body is the size of array with heroes
+ */
 let makeTableMarkup = () => {
   let divPlaceholder = document.getElementById(`placeholder`);
+  //remove placeholder value
+  divPlaceholder.innerHTML = ``;
+  // start making table markup
   let table = divPlaceholder.appendChild(document.createElement(`table`))
   let thead = table.appendChild(document.createElement(`thead`));
+  let tbody = table.appendChild(document.createElement(`tbody`));
   let trThead = thead.appendChild(document.createElement(`tr`));
 
-  for (let i = Object.keys(heroesList[0]).length; i > 0; i--) {
+  //make table header with buttons
+  for (let i = 0; i < Object.keys(heroesList[0]).length; i++) {
     let tdTrThead = trThead.appendChild(document.createElement(`td`));
     let input = tdTrThead.appendChild(document.createElement(`input`));
-    input.id = `${Object.keys(heroesList[0])[i - 1]}Btn`;
-    console.log(input);
+    input.id = `${Object.keys(heroesList[0])[i]}Btn`;
+    input.setAttribute(`type`, `button`);
+    switch (i) {
+      case 0:
+        input.setAttribute(`onclick`, `sortTable(\'name\')`);
+        input.setAttribute(`value`, `Name`);
+        break;
+      case 1:
+        input.setAttribute(`onclick`, `sortTable(\'strength\')`);
+        input.setAttribute(`value`, `Strength`);
+        break;
+      default:
+        input.setAttribute(`onclick`, `sortTable(\'age\')`);
+        input.setAttribute(`value`, `Age`);
+        break;
+    }
+  }
+
+  //make table body with number of row in order of array length
+  for (let i = 0; i < heroesList.length; i++) {
+    let trTbody = tbody.appendChild(document.c§reateElement(`tr`));
+    for (let j = 0; j < Object.keys(heroesList[0]).length; j++) {
+      let tdTrTbody = trTbody.appendChild(document.createElement(`td`));
+    }
   }
   table.id = `heroTable`;
-  console.log(table);
+}
+
+let tableFill = () => {
+  let tbody = document.getElementsByTagName("tbody");
+  for (let i = 0; i < tbody.rows.length; i++) {
+    for (let j = 0; j < tbody.rows[i].cells.length; j++) {
+      tbody.rows[i].cells[j].innerHTML = heroesList[i][j];
+    }
+  }
 }
 
 let makeTable = () => {
   makeNotification(`start`,);
   makeTableMarkup();
+  tableFill();
 }
 
 makeTable();
