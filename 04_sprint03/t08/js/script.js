f6bc5a59f44f35a2f4e4f5b70e0ed01d91635b3c
jacomogrelli <nechaeff@gmail.com>
2020-08-26 13:18:33 +0300

    t08

--- 04_sprint03/t08/js/script.js
+++ 04_sprint03/t08/js/script.js
@@ -12,19 +12,27 @@ let heroesList = [
   {name: 'Yon-Rogg', strength: 73, age: 52,},
 ]
 
+let ageFlag = false;
+let strengthFlag = false;
+let nameFlag = false;
+
 /*
  * Function makeNotification render notification div with the parameters:
  * order == start - generate starting div with the info how to sort table
  * order == ACS || DESC generate after sorting notification with the order
  * of sorting and witch parameter was used
  */
-let makeNotification = (order, param) => {
+let makeNotification = (param) => {
   let divNotification = document.getElementById(`notification`);
   let node = document.createElement(`span`);
-  if (order === `start`)
+  if (param === `start`)
     node.innerHTML = `Click on cell to sort the column`;
   else
-    node.innerHTML = `Sorting by ${param}, order: ${order}`;
+    node.innerHTML = `Sorting by ${param}, order: ${
+      (param === `Name`) ? (nameFlag ? `ASC` : `DESC`) : 
+        (param === `Age`) ? (ageFlag ? `ASC` : `DESC`) :
+          (strengthFlag ? `ASC` : `DESC`)
+  }`;
   divNotification.innerHTML = ``;
   divNotification.appendChild(node);
 }
@@ -51,15 +59,15 @@ let makeTableMarkup = () => {
     input.setAttribute(`type`, `button`);
     switch (i) {
       case 0:
-        input.setAttribute(`onclick`, `sortTable(\'name\')`);
+        input.setAttribute(`onclick`, `sortTable(\'Name\')`);
         input.setAttribute(`value`, `Name`);
         break;
       case 1:
-        input.setAttribute(`onclick`, `sortTable(\'strength\')`);
+        input.setAttribute(`onclick`, `sortTable(\'Strength\')`);
         input.setAttribute(`value`, `Strength`);
         break;
       default:
-        input.setAttribute(`onclick`, `sortTable(\'age\')`);
+        input.setAttribute(`onclick`, `sortTable(\'Age\')`);
         input.setAttribute(`value`, `Age`);
         break;
     }
@@ -67,7 +75,7 @@ let makeTableMarkup = () => {
 
   //make table body with number of row in order of array length
   for (let i = 0; i < heroesList.length; i++) {
-    let trTbody = tbody.appendChild(document.c§reateElement(`tr`));
+    let trTbody = tbody.appendChild(document.createElement(`tr`));
     for (let j = 0; j < Object.keys(heroesList[0]).length; j++) {
       let tdTrTbody = trTbody.appendChild(document.createElement(`td`));
     }
@@ -77,15 +85,47 @@ let makeTableMarkup = () => {
 
 let tableFill = () => {
   let tbody = document.getElementsByTagName("tbody");
-  for (let i = 0; i < tbody.rows.length; i++) {
-    for (let j = 0; j < tbody.rows[i].cells.length; j++) {
-      tbody.rows[i].cells[j].innerHTML = heroesList[i][j];
-    }
+  for (let i = 0; i < heroesList.length; i++) {
+    tbody[0].rows[i].cells[0].textContent = heroesList[i].name;
+    tbody[0].rows[i].cells[1].textContent = heroesList[i].strength;
+    tbody[0].rows[i].cells[2].textContent = heroesList[i].age;
   }
 }
 
+let sortTable = (param) => {
+  if (param === `Age`) {
+    if (ageFlag === false) {
+      ageFlag = true;
+      heroesList.sort((a, b) => a.age > b.age ? 1 : -1);
+    } else {
+      ageFlag = false;
+      heroesList.sort((a, b) => a.age > b.age ? -1 : 1);
+    }
+  }
+  if (param === `Strength`) {
+    if (strengthFlag === false) {
+      strengthFlag = true;
+      heroesList.sort((a, b) => a.strength > b.strength ? 1 : -1);
+    } else {
+      strengthFlag = false;
+      heroesList.sort((a, b) => a.strength > b.strength ? -1 : 1);
+    }
+  }
+  if (param === `Name`) {
+    if (nameFlag === false) {
+      nameFlag = true;
+      heroesList.sort((a, b) => a.name > b.name ? 1 : -1);
+    } else {
+      nameFlag = false;
+      heroesList.sort((a, b) => a.name > b.name ? -1 : 1);
+    }
+  }
+  makeNotification(param)
+  tableFill();
+}
+
 let makeTable = () => {
-  makeNotification(`start`,);
+  makeNotification(`start`);
   makeTableMarkup();
   tableFill();
 }
