8a2b46dfddd1bc69f5cdb0a0789b9519af2628d7
jacomogrelli <nechaeff@gmail.com>
2020-08-31 19:24:26 +0300

    evening commit

--- 04_sprint03/t09/js/script.js
+++ 04_sprint03/t09/js/script.js
@@ -9,6 +9,14 @@
 let playBox = [];
 
 /*
+ * Variable that contains information about number of turns, additional
+ * information is what player have to turn
+ * if even = Player 1
+ * if odd = Player 2
+ */
+let turnsCounter = 0;
+
+/*
  * Function fill array for results with 0 when new game has been started
  */
 let playBoxZeroFill = () => {
@@ -18,7 +26,6 @@ let playBoxZeroFill = () => {
       playBox[i][j] = 0;
   }
 }
-
 
 /*
  * Function change background colors of different blocks
@@ -52,3 +59,67 @@ let changeBackground = (id) => {
     infoBar.style.backgroundColor = `#cd903c`;
   }
 }
+
+let winDrawChecker = () => {
+  //check rows for win
+  for (let i = 0; playBox[i]; i++) {
+    if ((playBox[i][0] === playBox[i][1] && playBox[i][0] === playBox[i][2]
+      && playBox[i][0] !== 0)
+      || (playBox[0][i] === playBox[1][i] && playBox[0][i] === playBox[2][i]
+      && playBox[i][0] !== 0)) {
+      let result = document.querySelector(`#resultInfoBar`);
+      result.innerHTML = `Player ${playBox[i][0]} won!`;
+      changeBackground(3);
+      listener(false);
+    }
+  }
+}
+
+let makeTurn = (str) => {
+  let divArea = document.querySelector(`#${str}`);
+  let element = document.createElement(`div`);
+  let turnCounterArea = document.querySelector(`#nmbrTurnCounter`);
+
+  if (turnsCounter % 2 === 0) {
+    element.className = `cross`;
+    changeBackground(2);
+    playBox[+str.charAt(4) - 1][+str.charAt(6) - 1] = 1;
+  }
+  if (turnsCounter % 2 !== 0) {
+    element.className = `circle`;
+    changeBackground(1);
+    playBox[+str.charAt(4) - 1][+str.charAt(6) - 1] = 2;
+  }
+  divArea.appendChild(element);
+  turnsCounter += 1;
+  turnCounterArea.innerHTML = `${turnsCounter}`;
+  winDrawChecker();
+}
+
+let listener = (flag) => {
+  let areas = document.querySelectorAll(`.areas`);
+  let reset = document.querySelector(`#gameResetBtn`)
+
+  if (flag) {
+    for (let i = 0; areas[i]; i++)
+      areas[i].addEventListener(`click`, () => {
+        makeTurn(areas[i].getAttribute(`id`))
+      }, {once: true});
+    reset.addEventListener(`click`, () => {
+      window.location.reload()
+    })
+  } else {
+    for (let i = 0; areas[i]; i++)
+      areas[i].removeEventListener(`click`, () => {
+        makeTurn(areas[i].getAttribute(`id`))
+      });
+  }
+}
+
+let intMain = () => {
+  playBoxZeroFill();
+  listener(true);
+  changeBackground(1);
+}
+
+intMain();
