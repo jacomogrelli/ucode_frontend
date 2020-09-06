0750a8d1eebaad1ba2311bc225d262bef66bca99
jacomogrelli <nechaeff@gmail.com>
2020-09-06 18:51:47 +0300

    t12. sprint04 is finished

--- 04_sprint03/t11/js/script.js
+++ 04_sprint03/t11/js/script.js
@@ -1,19 +1,51 @@
 "use strict"
 
-let cookiesCount = 0;
+/*
+ * Function fill the Archive block with cookies content
+ */
+let archiveRerender = () => {
+  let textArea = document.querySelector(`#notesTextArea`);
+  let cookiesArray = (document.cookie).split(`delimiter`);
 
-let addCookies = () => {
-  let note = cookiesText.value.trim();
-  let expDate = new Date();
-
-  expDate.setDate(expDate.getDate() + 30)
-
-
-  if (note.length === 0) {
-    console.log(`It's empty. Try to input something in "Text input".`)
+  textArea.innerHTML = ``; // clear previous content of div
+  cookiesText.value = ``;  // clear previous content of input block
+  if (cookiesArray.length === 1) {
+    // if cookies are empty - filling with empty context
+    let empty = document.createElement(`span`); //
+    empty.innerHTML = `[empty]`
+    textArea.appendChild(empty);
     return;
   }
-  cookiesCount += 1;
-  document.cookie = (`note=${note}; expires=${expDate.toUTCString()}; path=/`);
-  console.log(document.cookie);
+  // create the list of spans with the cookies content
+  for (let i = 1; cookiesArray[i]; i++) {
+    let span = document.createElement(`span`);
+    span.innerHTML = `--> ${cookiesArray[i]}`;
+    textArea.appendChild(span);
+  }
 }
+
+/*
+ * Function add to cookies content of input text area to one cookie separated
+ * by "delimiter" string (for split string to array in archiveRerender function
+ */
+let addCookies = () => {
+  let note = cookiesText.value.trim();
+  let expDate = new Date(); // get current date
+  expDate.setDate(expDate.getDate() + 30); // set exploration + 30 days
+
+  if (note.length === 0) {
+    alert(`It's empty. Try to input something in "Text input".`)
+    return;
+  }
+  document.cookie = `${document.cookie}delimiter${note}; expires=${expDate.toUTCString()}; path=/`
+  archiveRerender();
+}
+
+let clearCookies = () => {
+  if (confirm(`Are you sure?`))
+    document.cookie = `=2; max-age=-1; path=/`
+  archiveRerender();
+}
+
+// for first filling of archive area block
+archiveRerender();
