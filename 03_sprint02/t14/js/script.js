1995c6395ba52d9ea7b00ddd1d7cf092e869cd8b
jacomogrelli <nechaeff@gmail.com>
2020-09-18 16:07:07 +0300

    evening commit

--- 03_sprint02/t14/js/script.js
+++ 03_sprint02/t14/js/script.js
@@ -18,6 +18,6 @@ let total = (addCount, addPrice, curentTotal) => {
 
 
 // let sum = total(1, 0.1);
-// sum= total(1, 0.2, sum);
-// sum= total(1, 0.78, sum);
+// sum = total(1, 0.2, sum);
+// sum = total(1, 0.78, sum);
 // console.log(sum); // will return 1.08
