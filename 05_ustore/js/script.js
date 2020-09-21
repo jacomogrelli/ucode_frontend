f4f54e145f05a4c420cb5247665cd48da8af8b04
jacomogrelli <nechaeff@gmail.com>
2020-09-21 16:16:36 +0300

    evening commit

--- 05_ustore/js/script.js
+++ 05_ustore/js/script.js
@@ -1,3 +1,19 @@
 "use strict"
 
+async function catalog() {
+  let result;
+  let json = fetch(`../assets/catalog/catalog.json`)
+    .then(response => response.json())
+    .then(data => result = data.catalog)
+  return Promise.resolve(result).then(() => {
+    console.log(result);
+    return result;
+  })
+}
+
 let pageReload = () => window.location.reload();
+
+let catalogRender = () => {
+
+}
+catalog();
