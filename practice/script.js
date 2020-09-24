c7df13b08f465f358ef97eb15e2b3796c17e8fb9
jacomogrelli <nechaeff@gmail.com>
2020-09-24 09:34:37 +0300

    evening commit

--- practice/script.js
+++ practice/script.js
@@ -1,25 +1,43 @@
 `use strict`
 
-const url = `https://jsonplaceholder.typicode.com/users`
+const requestURL = `https://jsonplaceholder.typicode.com/users`
 
-const xhr = new XMLHttpRequest()
+function sendRequest(method, url, body = null) {
+  return new Promise((resolve, reject) => {
+    //creat new request
+    const xhr = new XMLHttpRequest()
+    //open url
+    xhr.open(method, url)
+    //set type of response to JSON
+    xhr.responseType = `json`
+    //set type of POST method to json (default - string)
+    xhr.setRequestHeader(`Content-Type`, `application/json`)
+    //if loading successful
+    xhr.onload = () => {
+      //if loaded with successful code, but in
+      if (xhr.status >= 400) {
+        reject(xhr.response)
+      } esle {
+        resolve(xhr.response)
+      }
+    }
 
-xhr.open(`GET`, url)
+    xhr.onerror = () => {
+      console.log(xhr.response)
+    }
 
-xhr.responseType = `json`
-
-xhr.onload = () => {
-  if (xhr.status >= 400) {
-    console.error(xhr.response)
-  }
-  esle
-  {
-    console.log(xhr.response)
-  }
+    xhr.send(JSON.stringify(body))
+  })
 }
+//
+// sendRequest(`GET`, requestURL)
+//   .then(data => console.log(data))
+//   .catch(err => console.error(err))
 
-xhr.onerror = () => {
-  console.log(xhr.response)
-}
 
-xhr.send()
+sendRequest(`POST`, requestURL, {
+  name: `Oleksii`,
+  age: 26
+})
+  .then(data => console.log(data))
+  .catch(err => console.error(err))
