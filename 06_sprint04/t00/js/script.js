76660e32c7f77ccc8584816edea84d761bc0282a
jacomogrelli <nechaeff@gmail.com>
2020-09-28 11:51:06 +0300

    evening commit

--- 06_sprint04/t00/js/script.js
+++ 06_sprint04/t00/js/script.js
@@ -12,7 +12,9 @@ const magician = {
   }
 };
 
-function Creator (name, age, species, portrait) {
+let firstTime = true;
+
+function Creature(name, age, species, portrait) {
   this.name = name;
   this.age = age;
   this.species = species;
@@ -20,19 +22,54 @@ function Creator (name, age, species, portrait) {
   this.sayHello = () => console.log(`Hello, my name is ${this.name}`);
 }
 
-function Human (name, age, species, portrait, job) {
-  Creator.call(this, name, age, species, portrait);
+function Human(name, age, species, portrait, job) {
+  Creature.call(this, name, age, species, portrait);
   this.job = job;
 }
 
-function Dog (name, age, species, portrait, color) {
-  Creator.call(this, name, age, species, portrait);
-  this.color = color;
-}
-
-function Vampire (name, age, species, portrait, job, title) {
+function Vampire(name, age, species, portrait, job, title) {
   Human.call(this, name, age, species, portrait, job);
   this.title = title;
 }
 
+function Dog(name, age, species, portrait, color) {
+  Creature.call(this, name, age, species, portrait);
+  this.color = color;
+}
+
+let applyProperties = (object, button) => {
+  // let properties = document.querySelector("#properties"),
+  //     head;
+  //
+  // if (firstTime) {
+  //   document.querySelector()
+  // }
+  // console.log(object);
+}
+
+let changeStatus = (button) => {
+  switch (button.innerHTML.charAt(0)) {
+    case "h":
+      applyProperties(human, button);
+      break;
+    case "v":
+      applyProperties(vampire, button);
+      break;
+    case "d":
+      applyProperties(dog, button);
+      break;
+    case "n":
+    default:
+      applyProperties(undefined, button);
+      break;
+  }
+}
+
+
+let human = new Human("Linda", 22, "human", './assets/images/human.png', "doctor")
+let vampire = new Vampire("Vlad III", 915, "vampire", './assets/images/vampire.png', "unemployment", "count")
+let dog = new Dog("Fluffy", 3, "dog", './assets/images/dog.png', "brown")
+
+
+// changeStatus()
 
