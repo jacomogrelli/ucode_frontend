6b2c6de9f247ac93a16d1d0c554b09d2ecda7d50
jacomogrelli <nechaeff@gmail.com>
2020-10-07 15:08:20 +0300

    t04

---
+++ 06_sprint04/newtask.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+
+
+CURRENTDATE=`date +"%Y/%m/%d %H:%M"`
+
+mkdir -p t$1 t$1/assets t$1/css t$1/script
+touch t$1/css/style.css t$1/js/script.js t$1/index.html
+echo "<!-- ----------------------------------------------------------------------- -->
+<!--                                                                         -->
+<!--   $2. sprint04. t$1                           -->
+<!--                                                                         -->
+<!--   By: Oleksiy Nechaiev <nechaeff@gmail.com>                             -->
+<!--                        <t.me/losini>                                    -->
+<!--   Created: $CURRENTDATE (24H)                                       -->
+<!--   Finished: UNFINISHED                                                  -->
+<!--                                                                         -->
+<!--   ucode IT academy <ucode.world>                                        -->
+<!--   Je sues 42, nous sommes 42                                            -->
+<!--                                                                         -->
+<!--   Topics: $3                          -->
+<!--                                                                         -->
+<!-- ----------------------------------------------------------------------- -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>$2</title>
+  <meta charset=\"utf-8\">
+  <meta name=\"description\" content=\"t$1. $2\">
+  <meta name=\"author\" content=\"Oleksiy Nechayev\">
+  <meta name=\"keywords\" content=\"front-end, frontend, html,
+java script, javascript js, c, css, html5, css3\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale-1\">
+  <link rel=\"stylesheet\" href=\"./css/style.css\">
+  <script src=\"js/script.js\" defer></script>
+</head>
+
+<body>
+  <h1>$2</h1>
+</body>
+
+</html>" >> t$1/index.html
+
+echo "/* ----------------------------------------------------------------------- */
+/*                                                                         */
+/*   $2. sprint04. t$1                           */
+/*                                                                         */
+/*   By: Oleksiy Nechaiev <nechaeff@gmail.com>                             */
+/*                        <t.me/losini>                                    */
+/*   Created: $CURRENTDATE (24H)                                       */
+/*   Finished: UNFINISHED                                                  */
+/*                                                                         */
+/*   ucode IT academy <ucode.world>                                        */
+/*   Je sues 42, nous sommes 42                                            */
+/*                                                                         */
+/*   Topics: $3                          */
+/*                                                                         */
+/* ----------------------------------------------------------------------- */" >> t$1/css/style.css
+
+echo "/* ----------------------------------------------------------------------- */
+/*                                                                         */
+/*   $2. sprint04. t$1                           */
+/*                                                                         */
+/*   By: Oleksiy Nechaiev <nechaeff@gmail.com>                             */
+/*                        <t.me/losini>                                    */
+/*   Created: $CURRENTDATE (24H)                                       */
+/*   Finished: UNFINISHED                                                  */
+/*                                                                         */
+/*   ucode IT academy <ucode.world>                                        */
+/*   Je sues 42, nous sommes 42                                            */
+/*                                                                         */
+/*   Topics: $3                          */
+/*                                                                         */
+/* ----------------------------------------------------------------------- */
+'use strict'" >> t$1/js/script.js
---
+++ 06_sprint04/t04/js/script.js
@@ -0,0 +1,197 @@
+/* -------------------------------------------------------------------------- */
+/*                                                                            */
+/*   Mix-magician. sprint04. t04                                              */
+/*                                                                            */
+/*   By: Oleksiy Nechaiev <nechaeff@gmail.com>                                */
+/*                        <t.me/losini>                                       */
+/*   Created: 2020/10/07 09:47 (24H)                                          */
+/*   Finished: UNFINISHED                                                     */
+/*                                                                            */
+/*   ucode IT academy <ucode.world>                                           */
+/*   Je sues 42, nous sommes 42                                               */
+/*                                                                            */
+/*   Topics: JavaScript Class inheritance. Mixins                             */
+/*                                                                            */
+/* -------------------------------------------------------------------------- */
+'use strict'
+
+/*====== Functions ======*/
+/* Get element by selector, returns the element
+ * @id - css-style string for querySelector */
+let get = (id) => document.querySelector(id);
+let werewolfMixin = {
+  howl() {console.log(`ARH-WOOOOOOOOOOOOOOOOOOOO`)}
+}
+
+
+/* const object from ucode pdf, everytime click on buttons change prototype to
+ * active object human, dog, vampire or to Object.prototype if there is no
+ * prototype */
+const magician = {
+  /* Nahuya-to nado */
+  _hat: './assets/images/hat.png',
+  /* Function check if there is portrait link in object or return link to basic */
+  _getPortrait() {
+    if (this._portrait)
+      return this._portrait;
+    else
+      return './assets/images/magician.png';
+  },
+  'do magic'() {
+    console.log(`ABRACADABRA The prototype of ${this.name} is `);
+    console.log(Object.getPrototypeOf(this));
+  }
+};
+
+/* main Class, include general characteristics of object */
+class Creature {
+  constructor(options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.species = options.species;
+    this._portrait = options.portrait;
+  }
+
+  sayHello() {
+    console.log(`Hello, my name is ${this.name}`)
+  };
+}
+
+/* class for Human object. Parent prototype Creature connect with method
+ * call, which takes argument this and all others */
+class Human extends Creature {
+  constructor(options) {
+    super(options);
+    this.job = options.job
+  }
+}
+
+/* class for Vampire object */
+class Vampire extends Human {
+  constructor(options) {
+    super(options)
+    this.title = options.title
+  }
+}
+
+/* class fo Dog object */
+class Dog extends Creature {
+  constructor(options) {
+    super(options)
+    this.color = options.color
+  }
+}
+
+/* class for Werewolf */
+class Werewolf extends Human {
+  constructor(options) {
+    super(options)
+  }
+
+  transform() {
+    let head = get('#head')
+
+    /* toggle head img src attribute */
+    this._portrait = (this._portrait === `./assets/images/human.png`) ?
+      `./assets/images/werewolf.png` : `./assets/images/human.png`
+    head.setAttribute(`src`, `${this._portrait}`)
+  }
+}
+
+let human = new Human({
+  name: 'Linda',
+  age: 22,
+  species: 'human',
+  portrait: './assets/images/human.png',
+  job: 'doctor'
+})
+let vampire = new Vampire({
+  name: "Vlad III",
+  age: 915,
+  species: "vampire",
+  portrait: './assets/images/vampire.png',
+  job: "unemployment",
+  title: "count"
+})
+let dog = new Dog({
+  name: 'Fluffy',
+  age: 3,
+  species: 'dog',
+  portrait: './assets/images/dog.png',
+  color: 'unemployment'
+})
+let werewolf = new Werewolf({
+  name: 'Rachel',
+  age: 18,
+  species: 'werewolf',
+  portrait: './assets/images/human.png',
+  job: 'teacher',
+})
+
+
+/* mixing functions with prototype */
+Object.assign(Werewolf.prototype, werewolfMixin)
+
+/* Rendering main block
+ * @object - current object human, vampire, dog or Object.prototype if no
+ * prototype (make "this" back to magician)
+ * @button - button, that have been clicked (<button class="protoBtn"
+ * onclick="changeStatus(this)">human prototype</button> for example) */
+let applyProperties = (object, button) => {
+  let properties = get("#properties"),
+    head;
+  /* Apply to magician prototype of current object or back to magician if
+   @object = Object.prototype */
+  Object.setPrototypeOf(magician, object);
+  /* Apply class active to clicked button and remove previous */
+  if (button) {
+    get(".active").classList.remove("active")
+    button.classList.add("active")
+  }
+  /* Apply current portrait with magician._getPortrait function (see description
+   * in magician object) */
+  head = get('#head')
+    .setAttribute("src", `${magician._getPortrait()}`)
+  /* Add buttons and apply onclick function to buttons 'DO MAGIC' and
+   'SAY HELLO' and specific buttons for Werewolf */
+  properties.innerHTML = `<button id="do-magic" onclick="magician` +
+    `['do magic']()">DO MAGIC</button>`;
+  if (button && button.innerHTML != 'no prototype') {
+    properties.innerHTML += `<button id="say-hello"onclick="magician.sayHello()\
+">SAY HELLO</button>`
+    if (button.innerHTML === 'werewolf prototype')
+      properties.innerHTML += `<button id="transform" onclick="werewolf.\
+transform()">transform</button><button id="howl" onclick="werewolf.howl()">\
+howl</button>`
+  }
+
+  /* Render properties to span except _portrait */
+  for (const [key, value] of Object.entries(object))
+    if (key.charAt(0) !== "_")
+      properties.innerHTML += `<p class="property">${key}:` +
+        `<span class="propValue"> ${value}</span></p>`
+}
+
+/* Function choose object for rendering depends on clicked button
+ * @button - button, that have been clicked (<button class="protoBtn"
+ * onclick="changeStatus(this)">human prototype</button> for example) */
+let changeStatus = (button) => {
+  /* check was button clicked */
+  if (button) {
+    if (button.innerHTML.charAt(0) == "h")
+      applyProperties(human, button);
+    else if (button.innerHTML.charAt(0) == "v")
+      applyProperties(vampire, button);
+    else if (button.innerHTML.charAt(0) == "d")
+      applyProperties(dog, button);
+    else if (button.innerHTML.charAt(0) == "n")
+      applyProperties(Object.prototype, button);
+    else if (button.innerHTML.charAt(0) == "w")
+      applyProperties(werewolf, button)
+  }
+  /* if rendering for first time */
+  else
+    applyProperties(Object.prototype, button);
+}
+
+changeStatus();
