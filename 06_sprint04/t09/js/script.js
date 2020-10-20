6c64aedd80348cf365630f4d2eba56a43320a91d
jacomogrelli <nechaeff@gmail.com>
2020-10-20 10:07:40 +0300

    t09

--- 06_sprint04/t09/js/script.js
+++ 06_sprint04/t09/js/script.js
@@ -1,18 +1,18 @@
-/* ----------------------------------------------------------------------- */
-/*                                                                         */
-/*   Weather forecast. sprint04. t09                                       */
-/*                                                                         */
-/*   By: Oleksiy Nechaiev <nechaeff@gmail.com>                             */
-/*                        <t.me/losini>                                    */
-/*   Created: 2020/10/15 11:09 (24H)                                       */
-/*   Finished: UNFINISHED                                                  */
-/*                                                                         */
-/*   ucode IT academy <ucode.world>                                        */
-/*   Je sues 42, nous sommes 42                                            */
-/*                                                                         */
-/*   Topics: API Introduction                          */
-/*                                                                         */
-/* ----------------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                                                                            */
+/*   Weather forecast. sprint04. t09                                          */
+/*                                                                            */
+/*   By: Oleksiy Nechaiev <nechaeff@gmail.com>                                */
+/*                        <t.me/losini>                                       */
+/*   Created: 2020/10/15 11:09 (24H)                                          */
+/*   Finished: 2020/10/20 10:03 (24H)                                         */
+/*                                                                            */
+/*   ucode IT academy <ucode.world>                                           */
+/*   Je sues 42, nous sommes 42                                               */
+/*                                                                            */
+/*   Topics: API Introduction                                                 */
+/*                                                                            */
+/* -------------------------------------------------------------------------- */
 'use strict'
 /*====== Functions ======*/
 /* get element by selector, returns the element
@@ -31,34 +31,56 @@ let createWithClass = (tag, clas, text) => {
   return div
 }
 
+/* render single day to forecast div
+ * @forecast - response from openweathermap for single day */
 let render = (forecast) => {
-  console.log(forecast)
+  let forecastDiv = get('.forecast')
   let date = new Date(forecast.dt * 1000)
-  console.log(date)
+  let weekDay = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+  let day = createWithClass('div', 'day')
+
+  day.insertAdjacentHTML("beforeend", `<span class="dayOfWeek">\
+${weekDay[date.getDay()]}</span>`)
+  day.insertAdjacentHTML('beforeend', `<span class="date">\
+${date.getDate()}.${+date.getMonth() + 1}</span>`)
+  day.insertAdjacentHTML('beforeend', `<img src="https://openweather\
+map.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="icon">`)
+  day.insertAdjacentHTML('beforeend', `<span class="temperature"><span>\
+${forecast.temp.day.toFixed(0)}</span>°C</span>`)
+  forecastDiv.append(day)
 }
 
+/* send API request, details bellow:
+ * &lat, &lng - coords getting from browser
+ * &exclude - exclude unuseful forecasts
+ * &cnt - numbers of days
+ * &units - units of response
+ * &appid - API key */
 let getForecast = async (lat, lng) => {
-  let forecast = fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=current,minutely,hourly,alerts&cnt=6&units=metric&appid=e13ffa4a7ab745bda274278988a240f6`)
+  let forecast = fetch(`https://api.openweathermap.org/data/2.5/onecall?\
+lat=${lat}&lon=${lng}&exclude=current,minutely,hourly,alerts&cnt=6&units=metric\
+&appid=e13ffa4a7ab745bda274278988a240f6`)
     .then(response => response.json())
-    .then(data => data.daily.map((day) => {
-      render(day)
-    }))
+    .then(data => {
+      /* add city name */
+      get('.forecast').append(createWithClass('div', 'city', `${data.timezone}`))
+      /* render all daily forecast from array */
+      data.daily.map((day) => render(day))
+    })
 }
 
+/* get current location and send request */
 let start = async () => {
   try {
+    /* check is there in browser Geolocation API */
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         getForecast(position.coords.latitude, position.coords.longitude)
       })
+      /* if not - make forecast request with coord of Kyiv, Ukraine */
     } else {
-      getForecast(50.45466,30.5238)
+      getForecast(50.45466, 30.5238)
     }
-    // console.log(lat, lng)
-    // let forecast = await fetch('https://extreme-ip-lookup.com/json/')
-    //   .then(response => response.json())
-    //   .then(data => data)
-    // console.log(forecast)
   } catch (e) {
     console.log('Error is' + e)
   }
