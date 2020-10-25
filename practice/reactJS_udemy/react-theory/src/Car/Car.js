0430495d20d7ebb1676895fc87be4e25d2eed025
jacomogrelli <nechaeff@gmail.com>
2020-10-25 14:22:11 +0200

    evening commit

---
+++ practice/reactJS_udemy/react-theory/src/Car/Car.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//
+// function Car() {
+//   return (
+//     <h1>This is car component</h1>
+//   )
+// }
+
+// const Car = () => <h1>This is car component</h1>;
+
+
+// export default Car
+
+export default () => <h1>This is car component</h1>
